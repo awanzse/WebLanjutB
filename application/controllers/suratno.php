1a811c22148ad30f0570ce3e85778c414be23191
anno pujakesuma <annopujakesuma@gmail.com>
2019-05-17 23:43:58 +0700

    perubahan pada view

--- application/controllers/suratno.php
+++ application/controllers/suratno.php
@@ -20,9 +20,6 @@ class suratno extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view("data_view");
 	}
-	public function Biodata()
-	{
-		echo (
 }
