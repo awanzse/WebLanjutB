365b8416767996bc5eca713a619470228f109acc
suci primadona <50509986+Suciprimadona@users.noreply.github.com>
2019-05-20 07:29:59 +0700

    Add files via upload

---
+++ suci.php
@@ -0,0 +1,11 @@
+@@ -20,9 +20,6 @@ class suci extends CI_Controller {
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+		$this->load->view("data_view");
+	}
+	public function Biodata()
+	{
+		echo (
+}
