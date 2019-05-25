ff395351fa87a9491a0938e2e47fcb01948a2b32
Anno Pujakesuma <annopujakesuma@gmail.com>
2019-05-25 11:30:38 +0700

    cek

---
+++ application/views/cek_view.php
@@ -0,0 +1,173 @@
+<html>
+
+<head>
+
+<title>TABEL BIODATA</title>
+
+<style type=”text/css”>
+
+th{
+
+background-color:Blue;
+
+color:white;
+
+}
+
+</style>
+
+<style type=”text/css”>
+
+td{
+
+background-color:cyan;
+
+color:red;
+
+}
+
+</style>
+
+<style type=”text/css”>
+
+.baris-ganjil{
+
+background-color:yellow;
+
+color:blue;
+
+}
+
+</style>
+
+</head>
+
+<center>
+
+<body style=”background-image:url(background.jpg)”>
+
+<h1>BIODATA DIRI : </h1>
+
+<table style=”width:600px;” border=”1″ align=”center”>
+
+<tr style=”background-color:GREEN”>
+
+<th>BIODATA</th>
+
+<th>KETERANGAN</th>
+
+</tr>
+
+<tr>
+
+<td>Nama</td><td style=”text-align:left”>SURATNO</td>
+
+</tr>
+
+<tr>
+
+<td class=”baris-ganjil”>NO HP</td><td class=”baris-ganjil” style=”text-align:left”>081365022552</td>
+
+</tr>
+
+<tr>
+
+<td>Nim</td><td style=”text-align:left”>1637052</td>
+
+</tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Jurusan</td><td class=”baris-ganjil” style=”text-align:left”>Teknik Informatika</td>
+
+</tr>
+
+<tr>
+
+<td>Jenjang</td><td style=”text-align:left”>S1</td>
+
+</tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Jenis Kelamin</td><td class=”baris-ganjil” style=”text-align:left”>Laki-Laki</td>
+
+</tr>
+
+<tr>
+
+<td>Cita-cita</td><td style=”text-align:left”>Menjadi Guru</td>
+
+</tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Agama</td><td class=”baris-ganjil” style=”text-align:left”>ISLAM</td>
+
+<tr>
+
+<tr>
+
+<td>TTL</td><td style=”text-align:left”>Buntu Pane, 30 Januari 1984</td>
+
+<tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Golongan Darah</td><td class=”baris-ganjil” style=”text-align:left”> A </td>
+
+<tr>
+
+<tr>
+
+<td>Tinggi / Berat Badan</td><td style=”text-align:left”> 155 cm / 55 Kg </td>
+
+<tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Email </td><td class=”baris-ganjil” style=”text-align:left”>annopujakesuma@gmail.com</td>
+
+<tr>
+
+<td>Alamat Domisili</td><td style=”text-align:left”>Jl. Tuanku Tambusai - Padang Luhong Desa Rambah Tengah  Hilir</td>
+
+</tr>
+
+<tr>
+
+<td>Pekerjaan</td><td style=”text-align:left”>Guru</td>
+
+</tr>
+
+<tr>
+
+<td class=”baris-ganjil” >Photo</td><td class=”baris-ganjil” style=”text-align:left”><img><img src = “tika.jpg” width = “90 px” height = “150” ></img></td>
+
+</tr>
+
+<tr>
+
+<td>Perguruan Tinggi</td><td style=”text-align:left”><a href =”www.upp.ac.id”> Universitas Pasir Pengaraian</a></td>
+
+</tr>
+
+<tr style=”text-align:right;background-color:green;font-weight:bold”>
+
+</tr>
+
+<table>
+
+<a href =”home.html”> |Back to Home | </a>
+
+</center>
+
+</body>
+
+<footer>
+
+<marquee><font face = “Calibri” Color = “Blue” Font Size = “small”> Presented by Suratno </marquee>
+
+</footer>
+
+</html>
