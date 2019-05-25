e65fd276779796dd470674f8c715a8e4563ee6b3
aisyahnst <nasutionaisyah98@gmail.com>
2019-05-25 14:35:40 -0700

    aisyah

---
+++ application/views/aisyah_view.php
@@ -0,0 +1,105 @@
+<html>
+  <head>
+    <title>PROFIL MAHASISWA</title> 
+  </head>
+  <body bgcolor="#black" width="800px"> 
+  <div align="center">
+  
+<center>
+<h1>Curiculum Vitae</h1>
+    <table border="1">
+	<tr style="background-color:green">
+  
+ <td colspan="3"><center>BIODATA</center></td>
+ </tr>
+ <tr>
+ <td>NIM</td>
+ <td>:</td>
+ <td>1637028</td>
+ </tr>
+ <tr>
+ <td>NAMA</td>
+ <td>:</td>
+ <td>SITI AISYAH</td>
+ </tr>
+ <tr>
+ <td>SEMESTER/PRODI</td>
+ <td>:</td>
+ <td>6B/TEKNIK INFORMATIKA</td>
+ </tr>
+ <tr>
+ <td>MATA KULIAH</td>
+ <td>:</td>
+ <td>WEB LANJUT</td>
+ </tr>
+ <tr>
+ <td>DOSEN</td>
+ <td>:</td>
+ <td>INDRA GUNAWAN, S.Kom,.</td>
+ </tr>
+ </body>
+ </table>
+ </html>
+ 
+
+</center>
+<hr />
+<h2>BIODATA MAHASISWA</h2>
+
+<table width="800px"> 
+<tr>
+          <td width="25%">Nama</td>
+          <td width="1%">:</td>
+          <td><b>SITI AISYAH </b></td>
+        </tr>
+<tr>
+          <td>Tempat Tanggal, Lahir </td>
+          <td>:</td>
+          <td>Dalu-Dalu, 01 JUNI 1998</td>
+        </tr>
+<tr>
+          <td>Jenis Kelamin </td>
+          <td>:</td>
+          <td>Perempuan</td>
+        </tr>
+<tr>
+          <td>Agama </td>
+          <td>:</td>
+          <td>Islam</td>
+        </tr>
+<tr>
+          <td>Alamat</td>
+          <td>:</td>
+          <td>Jl.Transmigrasi, Lingkungan kuba Dalu-Dalu</td>
+        </tr>
+<tr>
+    <td>E-mail</td>
+          <td>:</td>
+          <td>nasutionaisyah98@gmail.com</td>
+        </tr>
+</tbody>
+    </table>
+<h2>Riwayat Pendidikan</h2>
+<table width="800px">
+      <tbody>
+<tr>
+          <td width="25%">2016-Sekarang</td>
+          <td width="1%">:</td>
+          <td>Fakultas Ilmu Komputer, Prodi Teknik Informatika, Universitas Pasir Pengaraian </td>
+        </tr>
+<tr>
+          <td>2013-2016</td>
+          <td>:</td>
+          <td>SMAN 3 Tambusai  </td>
+        </tr>
+<tr>
+          <td>2010-2013</td>
+          <td>:</td>
+          <td>MTsN Dalu-Dalu</td>
+<tr>
+          <td>2004-2010</td>
+          <td>:</td>
+          <td>SD Negeri 006 tambusai dalu dalu </td>
+<tr>
+          
+</table>
