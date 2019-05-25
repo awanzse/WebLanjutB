0cad517326ad22228b6a1fc2ffb9b3cae0e51109
ripatullahrahman1 <rivatullah02@gmail.com>
2019-05-25 15:14:32 +0700

    update riva

---
+++ application/views/riva.php
@@ -0,0 +1,106 @@
+<html>
+  <head>
+    <title>PROFIL MAHASISWA</title> 
+  </head>
+  <body bgcolor="#white" width="800px"> 
+  <div align="center">
+  
+<center>
+<h1>Curiculum Vitae</h1>
+    <table border="1">
+	<tr style="background-color:red">
+  
+ <td colspan="3"><center>BIODATA</center></td>
+ </tr>
+ <tr>
+ <td>NIM</td>
+ <td>:</td>
+ <td>1637042</td>
+ </tr>
+ <tr>
+ <td>NAMA</td>
+ <td>:</td>
+ <td>RIPATULLAH RAHMAN</td>
+ </tr>
+ <tr>
+ <td>SEMESTER/PRODI</td>
+ <td>:</td>
+ <td>6B/TEKNIK INFORMATIKA</td>
+ </tr>
+ <tr>
+ <td>MATA KULIAH</td>
+ <td>:</td>
+ <td>WEB LANJUT</td>
+ </tr>
+ <tr>
+ <td>DOSEN</td>
+ <td>:</td>
+ <td>INDRA GUNAWAN, S.Kom,.</td>
+ </tr>
+ </body>
+ </table>
+ </html>
+ 
+
+</center>
+<hr />
+<h2>BIODATA MAHASISWA</h2>
+
+<table width="800px"> 
+<tr>
+          <td width="25%">Nama</td>
+          <td width="1%">:</td>
+          <td><b>RIPATULLAH RAHMAN </b></td>
+		  <img src="<?= base_url () ?>assets/img/riva.jpg" height="200px" width="150px"></td>
+        </tr>
+<tr>
+          <td>Tempat Tanggal, Lahir </td>
+          <td>:</td>
+          <td>CIKAMPAK, 30 MEY 1995</td>
+        </tr>
+<tr>
+          <td>Jenis Kelamin </td>
+          <td>:</td>
+          <td>LAKI-LAKI</td>
+        </tr>
+<tr>
+          <td>Agama </td>
+          <td>:</td>
+          <td>Islam</td>
+        </tr>
+<tr>
+          <td>Alamat</td>
+          <td>:</td>
+          <td>Jl.Simpang Lombok Desa Sungai Tapah Rokan Hilir Riau</td>
+        </tr>
+<tr>
+    <td>E-mail</td>
+          <td>:</td>
+          <td>rivatullah02@gmail.com</td>
+        </tr>
+</tbody>
+    </table>
+<h2>Riwayat Pendidikan</h2>
+<table width="800px">
+      <tbody>
+<tr>
+          <td width="25%">2016-Sekarang</td>
+          <td width="1%">:</td>
+          <td>Fakultas Ilmu Komputer, Prodi Teknik Informatika, Universitas Pasir Pengaraian </td>
+        </tr>
+<tr>
+          <td>2010-2013</td>
+          <td>:</td>
+          <td>SMK MUHAMMADIYAH 01 PUJUD</td>
+        </tr>
+<tr>
+          <td>2007-2010</td>
+          <td>:</td>
+          <td>SMPN 03 PUJUD</td>
+<tr>
+          <td>2001-2007</td>
+          <td>:</td>
+          <td>SD Negeri 02 118236 CIKAMPAK SUMUT</td>
+<tr>
+          
+</table>
