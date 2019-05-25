717ab93c39242413856cd24d7651a95e98273d88
Erna Armita Nst <ernaarmita@gmail.com>
2019-05-25 13:33:52 +0700

    Revert "Revert "linda""
    This reverts commit ff8b96b54606dd2cbe39d8b738fb03b8351ae47a.

---
+++ application/views/asmalinda.php
@@ -0,0 +1,131 @@
+<html>
+  <head>
+    <title>PROFIL MAHASISWA</title> 
+  </head>
+  <body bgcolor="#blue" width="800px"> 
+  <div align="center">
+  
+<center>
+<h1>BIODATA</h1>
+    <table border="1">
+	<tr style="background-color:yellow">
+  
+ <td colspan="3"><center>BIODATA</center></td>
+ </tr>
+ <tr>
+  <tr>
+  <tr style="background-color:green">
+ <td>NAMA</td>
+ <td>:</td>
+ <td>ASMA LINDA</td>
+ </tr>
+ <td>NIM</td>
+ <td>:</td>
+ <td>1637034</td>
+ </tr>
+ <tr>
+ <td>SEMESTER/PRODI</td>
+ <td>:</td>
+ <td>6B/TEKNIK INFORMATIKA</td>
+ <tr>
+ <td>FAKULTAS</td>
+ <td>:</td>
+ <td>ILMU KOMPUTER</td>
+ </tr>
+ <tr>
+ <td>MATA KULIAH</td>
+ <td>:</td>
+ <td>WEB LANJUT</td>
+ </tr>
+ <tr>
+ <td>DOSEN</td>
+ <td>:</td>
+ <td>INDRA GUNAWAN, S.Kom,.</td>
+ </tr>
+ </body>
+ </table>
+ </html>
+ 
+
+</center>
+<hr />
+<h2>BIODATA MAHASISWA</h2>
+
+<table width="800px"> 
+<tr>
+          <td width="25%">Nama</td>
+          <td width="1%">:</td>
+          <td><b>ASMA LINDA </b></td>
+        </tr>
+		<img src="<?= base_url () ?>assets/img/linda.jpg" height="200px" width="150px"></td>
+<tr>
+          <td>Tempat Tanggal, Lahir </td>
+          <td>:</td>
+          <td>KOTA TENGAH, 15 JULI 1997</td>
+        </tr>
+<tr>
+          <td>Jenis Kelamin </td>
+          <td>:</td>
+          <td>Perempuan</td>
+        </tr>
+<tr>
+          <td>Agama </td>
+          <td>:</td>
+          <td>Islam</td>
+        </tr>
+<tr>
+          <td>Alamat</td>
+          <td>:</td>
+          <td>KOTA TENGAH Jl.SYEKH ABDUL WAHAB ROKAN</td>
+        </tr>
+<tr>
+    <td>E-mail</td>
+          <td>:</td>
+          <td>asmalinda036@gmail.com</td>
+        </tr>
+<tr>
+    <td>Cita-Cita</td>
+          <td>:</td>
+          <td>Menjadi Orang Sukses</td>
+        </tr>
+<tr>
+    <td>Motto</td>
+          <td>:</td>
+          <td>Semua Hal Di Dunia Ini Bisa Kita Dapatkan Asal Kita Mau Berusaha</td>
+        </tr>
+<tr>
+    <td>Pesan/Kesan</td>
+          <td>:</td>
+          <td>Pergaulan Tidak Terbatas Oleh Apapun Namun Saya Hanya Ingin Memilih Pergaulan Yang Positif </td>
+        </tr>
+
+</tbody>
+    </table>
+<h2>Riwayat Pendidikan</h2>
+<table width="800px">
+      <tbody>
+<tr>
+          <td width="25%">2016-Sekarang</td>
+          <td width="1%">:</td>
+          <td>Fakultas Ilmu Komputer, Prodi Teknik Informatika, Universitas Pasir Pengaraian </td>
+        </tr>
+<tr>
+          <td>2013-2016</td>
+          <td>:</td>
+          <td>SMK N 1 RAMBAH </td>
+        </tr>
+<tr>
+          <td>2010-2013</td>
+          <td>:</td>
+          <td>MTS N KOTA TENGAH</td>
+<tr>
+          <td>2004-2010</td>
+          <td>:</td>
+          <td>SD Negeri 001 KOTA TENGAH </td>
+<tr>
+          <td>2003-2004</td>
+          <td>:</td>
+          <td>TK DARMAWANITA</td>
+        </tr>
+</tr>
+</table>
