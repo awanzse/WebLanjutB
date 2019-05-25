a1e05003e82fe95a941f530be25eb24f27fed1c2
Anno Pujakesuma <annopujakesuma@gmail.com>
2019-05-25 14:27:09 +0700

    Update data_view.php

--- application/views/data_view.php
+++ application/views/data_view.php
@@ -1,132 +1,72 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>UAS WEB LANJUT</title> 
-  </head>
-  	<body bgcolor="#green" width="800px">
-		<div class="navbar navbar-inverse navbar-fixed-top">
-		<header>
-		<img src="assets/image/suratno.jpg">
-		</header>
-		</div>
-        <center>
-</body>
-<div>
-<h1>BIODATA DIRI : </h1>
-
-<table bgcolor="#white" style=”width:600px;” border=”1″ align=”center”>
+<head>
+	<title>UAS WEBLANJUT</title>
+	<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css" >
+	<script src="<?= base_url() ?>assets/bootstrap/js/bootstrap.min.js" ></script>
+</head>
+<body>
+<div class="jumbotron text-center">
+  <img src="<?= base_url() ?>assets/image/suratno.jpg">
+  <h1>CURRICULUM VITAE</h1>
+  <p>Belajar web dengan menggunakan Code Igniter</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>DATA PRIBADI</h3>
+      <table style=”width:600px;” border=”1″ align=”center”>
 
 <tr>
 
 <th>BIODATA</th>
-
 <th>KETERANGAN</th>
-
 </tr>
-
 <tr>
-
 <td>Nama</td><td style=”text-align:left”>SURATNO</td>
-
-</tr>
-
-<tr>
-
-<td class=”baris-ganjil”>NO HP</td><td class=”baris-ganjil” style=”text-align:left”>081365022552</td>
-
-</tr>
-
-<tr>
-
-<td>Nim</td><td style=”text-align:left”>1637052</td>
-
-</tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Jurusan</td><td class=”baris-ganjil” style=”text-align:left”>Teknik Informatika</td>
-
-</tr>
-
-<tr>
-
-<td>Jenjang</td><td style=”text-align:left”>S1</td>
-
-</tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Jenis Kelamin</td><td class=”baris-ganjil” style=”text-align:left”>Laki-Laki</td>
-
-</tr>
-
-<tr>
-
-<td>Cita-cita</td><td style=”text-align:left”>Menjadi Guru</td>
-
-</tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Agama</td><td class=”baris-ganjil” style=”text-align:left”>ISLAM</td>
-
-<tr>
-
-<tr>
-
-<td>TTL</td><td style=”text-align:left”>Buntu Pane, 30 Januari 1984</td>
-
-<tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Golongan Darah</td><td class=”baris-ganjil” style=”text-align:left”> A </td>
-
-<tr>
-
-<tr>
-
-<td>Tinggi / Berat Badan</td><td style=”text-align:left”> 155 cm / 55 Kg </td>
-
-<tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Email </td><td class=”baris-ganjil” style=”text-align:left”>annopujakesuma@gmail.com</td>
-
-<tr>
-
-<td>Alamat Domisili</td><td style=”text-align:left”>Jl. Tuanku Tambusai - Padang Luhong Desa Rambah Tengah  Hilir</td>
-
-</tr>
-
-<tr>
-
-<td>Pekerjaan</td><td style=”text-align:left”>Guru</td>
-
-</tr>
-
-<tr>
-
-<td class=”baris-ganjil” >Photo</td><td class=”baris-ganjil” style=”text-align:left”> 
+<td rowspan="5">
 <img src="<?= base_url() ?>assets/img/logoupp100x100.png">
-</td>
-
 </tr>
-
 <tr>
-
-<td>Perguruan Tinggi</td><td style=”text-align:left”> Universitas Pasir Pengaraian</td>
-
+<td>Nim</td><td style=”text-align:left”>1637052</td>
 </tr>
-
-<tr style=”text-align:right;background-color:green;font-weight:bold”>
-
+<tr>
+<td class=”baris-ganjil”>Tempat Tgl Lahir</td><td class=”baris-ganjil” style=”text-align:left”>Buntu Pane, 30 Januari 1984</td>
 </tr>
-
-<table>
+<tr>
+<td class=”baris-ganjil”>Jenis Kelamin</td><td class=”baris-ganjil” style=”text-align:left”>Laki-laki</td>
+</tr>
+<tr>
+<td class=”baris-ganjil”>Agama</td><td class=”baris-ganjil” style=”text-align:left”>Islam</td>
+</tr>
+<tr>
+<td class=”baris-ganjil”>Alamat</td><td class=”baris-ganjil” style=”text-align:left”>RT. 04 RW. 02 Dusun Simpang Raya Desa Rambah Tengah Hilir</td>
+</tr>
+<tr>
+<td class=”baris-ganjil”>Handphone</td><td class=”baris-ganjil” style=”text-align:left”>081365022552</td>
+</tr>
+<tr>
+<td class=”baris-ganjil”>Email</td><td class=”baris-ganjil” style=”text-align:left”>annopujakesuma@gmail.com</td>
+</tr>
+</table>
+    </div>
+    <div class="col-sm-4">
+      <h3>PENDIDIKAN FORMAL</h3>
+      <p>1. SD Negeri 045 Pasir Pengaraian Masuk Tahun 1990 lulus Tahun 1996</p>
+      <p>2. SMP Negeri 1 Rambah Masuk Tahun 1996 lulus Tahun 1999</p>
+	  <p>3. SMK Negeri 2 Pekanbaru Masuk Tahun 2000 lulus Tahun 2003</p>
+	  <p>4. Universitas Pasir Pengaraian Jurusan T. Informatika Masuk Tahun 2016 sampai sekarang</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>PENGALAMAN KERJA</h3>        
+      <p>1. Sebagai Instruktur IKPI Komputer Pasir Pengaraian</p>
+      <p>2. Sebagai Pengajar di SMP Negeri 7 Rambah</p>
+	  <p>3. Sebagai Pengajar di SMA Negeri 2 Rambah Hilir</p>
+    </div>
+  </div>
 </div>
-                 </div>
-                <footer>                     
+<footer>                     
                    <div id="column">
                         <h2>Dapatkan Bantuan dan Dukungan</h2>
                         Untuk mendapatkan petunjuk atau dukungan dari kami, kunjungi situs <a href="#">www.annopujakesuma.com</a> dan submit form anda.
@@ -142,8 +82,6 @@
                         <a href="https://www.facebook.com/annopujakesuma">Facebook</a><br>
                         <a href="#">Twitter</a>
                     </div> 
-                </footer>
-            </div> 
-        </div>
-    </body>
+</footer>
+</body>
 </html>
