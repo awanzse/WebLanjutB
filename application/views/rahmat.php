6db623bd507dfce6598fdfcfce5349b64aab7239
rinaldi45 <50510028+rinaldi45@users.noreply.github.com>
2019-05-25 17:18:40 -0700

    biodata

--- application/views/rahmat.php
+++ application/views/rahmat.php
@@ -1,121 +1,111 @@
-<!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>WELCOME TO GIBHUB R.RINALDI</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <title>PROFIL MAHASISWA</title> 
+  </head>
+  <body bgcolor="#green" width="800px"> 
+  <div align="center">
+  
+<center>
+<h1>Curiculum Vitae</h1>
+    <table border="1">
+	<tr style="background-color:#blue">
+  
+ <td colspan="3"><center>BIODATA</center></td>
+ </tr>
+ <tr>
+ <td>NIM</td>
+ <td>:</td>
+ <td>1637032</td>
+ </tr>
+ <tr>
+ <td>NAMA</td>
+ <td>:</td>
+ <td>RAHMAT RINALDI</td>
+ </tr>
+ <tr>
+ <td>SEMESTER/PRODI</td>
+ <td>:</td>
+ <td>6B/TEKNIK INFORMATIKA</td>
+ </tr>
+ <tr>
+ <td>MATA KULIAH</td>
+ <td>:</td>
+ <td>WEB LANJUT</td>
+ </tr>
+ <tr>
+ <td>DOSEN</td>
+ <td>:</td>
+ <td>INDRA GUNAWAN, S.Kom,.</td>
+ </tr>
+ </body>
+ </table>
+ </html>
+ 
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <h1>WELCOME TO GIBHUB R.RINALDI</h1>
+</center>
+<hr />
+<h2>BIODATA MAHASISWA</h2>
 
-        <table>
-            <thead>
-                <tr>
-                    <th class="name-col">NAMA MURID</th>
-                    <th>1</th>
-                    <th>2</th>
-                    <th>3</th>
-                    <th>4</th>
-                    <th>5</th>
-                    <th>6</th>
-                    <th>7</th>
-                    <th>8</th>
-                    <th>9</th>
-                    <th>10</th>
-                    <th>11</th>
-                    <th>12</th>
-                    <th class="missed-col">HARI TIDAK DIKETAHUI</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="student">
-                    <td class="name-col">FRENGKY ALDERADO</td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="missed-col">0</td>
-                </tr>
-                <tr class="student">
-                    <td class="name-col">RAHMAT RINALDI</td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="missed-col">0</td>
-                </tr>
-                <tr class="student">
-                    <td class="name-col">RIDHO GUNAWAN</td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="missed-col">0</td>
-                </tr>
-                <tr class="student">
-                    <td class="name-col">PONING KAPALO</td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="missed-col">0</td>
-                </tr>
-                <tr class="student">
-                    <td class="name-col">RAJA CIKIDAU</td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="missed-col">0</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="js/app.js"></script>
-    </body>
-</html>
+<table width="800px"> 
+<tr>
+          <td width="25%">Nama</td>
+          <td width="1%">:</td>
+          <td><b>RAHMAT RINALDI</b></td>
+          <td rowspan="5">
+		  <img src="<?= base_url() ?>assets/img/rinaldi" height="200px" width="150px"></td>
+        </tr>
+<tr>
+          <td>Tempat Tanggal, Lahir </td>
+          <td>:</td>
+          <td>kumu,11 November 1997</td>
+        </tr>
+<tr>
+          <td>Jenis Kelamin </td>
+          <td>:</td>
+          <td>LAKI LAKI</td>
+        </tr>
+<tr>
+          <td>Agama </td>
+          <td>:</td>
+          <td>Islam</td>
+        </tr>
+<tr>
+          <td>Alamat</td>
+          <td>:</td>
+          <td>kumu,desa rambah,rambah hilir,riau.</td>
+        </tr>
+<tr>
+    <td>E-mail</td>
+          <td>:</td>
+          <td>rahmatrinaldi45@gmail.com</td>
+        </tr>
+</tbody>
+    </table>
+<h2>Riwayat Pendidikan</h2>
+<table width="800px">
+      <tbody>
+<tr>
+          <td width="25%">2016-Sekarang</td>
+          <td width="1%">:</td>
+          <td>Fakultas Ilmu Komputer, Prodi Teknik Informatika, Universitas Pasir Pengaraian </td>
+        </tr>
+<tr>
+          <td>2013-2016</td>
+          <td>:</td>
+          <td>SMAN2 RAMBAH HILIR</td>
+        </tr>
+<tr>
+          <td>2010-2013</td>
+          <td>:</td>
+          <td>SMPN 5 RAMBAH HILIR</td>
+<tr>
+          <td>2004-2010</td>
+          <td>:</td>
+          <td>SD Negeri 020 RAMBAH HILIR</td>
+</tr>
+</table>
+			<p>Uraian bioadata saya diatas pun dapat diakses di :
+			<ul>
+			<li><a href="https://web.facebook.com/meimer.alderado.5">facebook</a></li>
+			</ul>
+			</p>
