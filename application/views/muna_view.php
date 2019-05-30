fe3c6d75b9f7cc7ea93d9cfbf359976feb271e8c
munasitorus <50987900+panemuna@users.noreply.github.com>
2019-05-30 07:26:24 -0700

    munapane

--- application/views/muna_view.php
+++ application/views/muna_view.php
@@ -2,37 +2,37 @@
   <head>
     <title>PROFIL MAHASISWA</title> 
   </head>
-  <body bgcolor="#white" width="800px"> 
+  <body bgcolor="#green" width="800px"> 
   <div align="center">
   
 <center>
-<h1>Curiculum Vitae</h1>
+<h1><marquee behavior="alternate" width="500" height="40">Curriculum Vitae</marquee></h1>
     <table border="1">
-	<tr style="background-color:white">
+	<tr style="background-color:gray">
   
  <td colspan="3"><center>BIODATA</center></td>
  </tr>
- <tr>
+ <tr style="background-color:pink">
  <td>NIM</td>
  <td>:</td>
  <td>1637056</td>
  </tr>
- <tr>
+ <tr style="background-color:pink">
  <td>NAMA</td>
  <td>:</td>
  <td>MUNAWAROH SITORUS</td>
  </tr>
- <tr>
+ <tr style="background-color:pink">
  <td>SEMESTER/PRODI</td>
  <td>:</td>
  <td>6B/TEKNIK INFORMATIKA</td>
  </tr>
- <tr>
+ <tr style="background-color:pink">
  <td>MATA KULIAH</td>
  <td>:</td>
  <td>WEB LANJUT</td>
  </tr>
- <tr>
+ <tr style="background-color:pink">
  <td>DOSEN</td>
  <td>:</td>
  <td>INDRA GUNAWAN, S.Kom,.</td>
@@ -50,12 +50,10 @@
 <tr>
           <td width="25%">Nama</td>
           <td width="1%">:</td>
-          <td><b>MUNAWAROH SITORUS </b></td>
-        </tr>
-<tr>
-          <td>Tempat Tanggal, Lahir </td>
-          <td>:</td>
-          <td>teluk binjai, 22 MARET 1998</td>
+          <td><b>MUNAWAROH SITORUS</b></td>
+          <td rowspan="5">
+		  <img src="<?=base_url() ?>assets/img/muna.jpg" width="100px" height="150px"> 
+		  </td>
         </tr>
 <tr>
           <td>Jenis Kelamin </td>
@@ -107,3 +105,27 @@
         </tr>
 </tr>
 </table>
+
+
+
+
+<tr>
+<tr>
+ <footer>    
+<tr>
+<tr>
+                   <p><p align="center">copyright &copy; 2019 MUNAWAROH SITORUS</p></p>
+                    <div id="column">
+                       <h2>Komentar,saran,bantuan dan dukungan anda
+                    silahkan kirimkan di</h2>
+                        dalu-dalu, Pasir Pengaraian<br>
+                        Rokan Hulu, Riau<br>
+                        +628 7552 3556
+                    </div> 
+                    <div id="column">
+                        <h2>Temukan Kami di ...</h2>
+                        <a href="https://www.facebook.com/piny.alvionita"> Facebook </a><br>
+                        <a href="https://www.instagram.com/myuna_sitorus_22/"> Instagram </a><br>
+						<a href="https://mail.google.com/mail/u/0/#inbox"> Gmail </a>
+                    </div> 
+                </footer>
